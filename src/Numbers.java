b474dcdd1e87d7cbb21afe36c74570fc9ad23316
glueeeeed2 <zuzankauuuu@gmail.com>
2026-09-04 10:17:53 +0200

    refactor: prevent repetition numbers in array

--- src/Numbers.java
+++ src/Numbers.java
@@ -7,9 +7,22 @@ public class Numbers {
     public int[] draw() {
         Random random = new Random();
         int[] array = new int[6];
+        boolean isExists = false;
+
         for (int i = 0; i < array.length; i++)  {
             int randomNumber = random.nextInt(1,49);
-            array[i] = randomNumber;
+
+            for (int y = 0; y < array.length; y++) {
+                if (array[y] == randomNumber) {
+                    isExists = true;
+                    break;
+                }
+            }
+
+            if (!isExists) {
+                array[i] = randomNumber;
+            }
+
         }
         return array;
     }
